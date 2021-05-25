02a64913aad8f8a8bb93644e2d346ee2dde5905d
Hugo Hills <hugo.hills@regnosys.com>
2021-05-25 11:11:20 +0100

    Add validator

--- rosetta-source/src/main/java/com/regnosys/model/ModelRuntimeModule.java
+++ rosetta-source/src/main/java/com/regnosys/model/ModelRuntimeModule.java
@@ -1,18 +1,23 @@
 package com.regnosys.model;
 
 import com.google.inject.AbstractModule;
+import com.regnosys.rosetta.common.validation.RosettaTypeValidator;
+import com.rosetta.model.lib.validation.ModelObjectValidator;
 import demo.functions.functions.Create_Date;
 import demo.functions.functions.Create_DateImpl;
-import org.jetbrains.annotations.NotNull;
 
 public class ModelRuntimeModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		bind(ModelObjectValidator.class).to(bindModelObjectValidator());
 		bind(Create_Date.class).to(bindCreateDate());
 	}
 
-	@NotNull
+	protected Class<? extends ModelObjectValidator> bindModelObjectValidator() {
+		return RosettaTypeValidator.class;
+	}
+
 	protected Class<? extends Create_Date> bindCreateDate() {
 		return Create_DateImpl.class;
 	}
