b66ce46198cf1cc126fc9f570840e3c500eb77bb
David Al-Kanani <david.al-kanani@regnosys.com>
2022-11-22 11:39:51 +0000

    add qualification guice bindings

--- rosetta-source/src/main/java/com/regnosys/model/ModelRuntimeModule.java
+++ rosetta-source/src/main/java/com/regnosys/model/ModelRuntimeModule.java
@@ -1,9 +1,12 @@
 package com.regnosys.model;
 
 import com.google.inject.AbstractModule;
+import com.regnosys.rosetta.common.postprocess.qualify.QualificationHandlerProvider;
+import com.rosetta.model.lib.qualify.QualifyFunctionFactory;
 import com.rosetta.model.lib.validation.ValidatorFactory;
 import demo.emissions.functions.Create_Date;
 import demo.emissions.functions.Create_DateImpl;
+import org.isda.cdm.qualify.CdmQualificationHandlerProvider;
 
 public class ModelRuntimeModule extends AbstractModule {
 
@@ -11,6 +14,8 @@ public class ModelRuntimeModule extends AbstractModule {
 	protected void configure() {
 		bind(ValidatorFactory.class).to(bindValidatorFactory());
 		bind(Create_Date.class).to(bindCreateDate());
+		bind(QualifyFunctionFactory.class).to(bindQualifyFunctionFactory());
+		bind(QualificationHandlerProvider.class).to(bindQualificationConfigProvider());
 	}
 
 	protected Class<? extends ValidatorFactory> bindValidatorFactory() {
@@ -20,4 +25,13 @@ public class ModelRuntimeModule extends AbstractModule {
 	protected Class<? extends Create_Date> bindCreateDate() {
 		return Create_DateImpl.class;
 	}
+
+	protected Class<? extends QualifyFunctionFactory> bindQualifyFunctionFactory() {
+		return QualifyFunctionFactory.Default.class;
+	}
+
+	protected Class<? extends QualificationHandlerProvider> bindQualificationConfigProvider() {
+		return CdmQualificationHandlerProvider.class;
+	}
+
 }
